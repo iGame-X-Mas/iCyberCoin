e3fe4c71a322007b6ad0dc6709d0a9e169e66f1b
🅰️🅻🅴🆇 🎮 <demian.x.mas@gmail.com>
2025-03-20 19:21:47 +0200

    Trigger Description
    Added comment for character shrink and grow trigger

--- Assets/_G A M E/Scripts/Triggers/TriggerHeight/TriggerDecreaseHeight.cs
+++ Assets/_G A M E/Scripts/Triggers/TriggerHeight/TriggerDecreaseHeight.cs
@@ -7,8 +7,9 @@ using UnityEngine;
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) 
         {
+            // Scale down the player height while maintaining X and Z scales
             _transformPlayerDecreaseHeight.localScale = new Vector3(_transformPlayerDecreaseHeight.localScale.x, _minHeight, _transformPlayerDecreaseHeight.localScale.z);
         }
     }
