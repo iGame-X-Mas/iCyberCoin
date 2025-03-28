4ce13988349a6c3658a8534fee62012e5d019239
🅰️🅻🅴🆇 🎮 <demian.x.mas@gmail.com>
2025-03-28 16:41:04 +0200

    Optimizations And Fixes Code
    The change ensures "transform.position" is only updated if needed, reducing unnecessary calculations
    
    2.  Updated particle system in first scene

--- Assets/_G A M E/Scripts/Player/Player Move/RoadBoundary.cs
+++ Assets/_G A M E/Scripts/Player/Player Move/RoadBoundary.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class RoadBoundary : MonoBehaviour
 {
-    
-     private float maX = 2.5f;
-     private float minX = -2.5f;
-     private float maxZ = -30f;
+    // Define boundary limits for X and Z positions
+    private const float maxX = 2.5f;
+    private const float minX = -2.5f;
+    private const float maxZ = -30f;
 
     private void LateUpdate()
     {
@@ -14,11 +14,10 @@ public class RoadBoundary : MonoBehaviour
 
     private void ClampPosition()
     {
-        Vector3 position = transform.position;
-        transform.position = position;
+            Vector3 position = transform.position; // Get the current position of the object
+            position.x = Mathf.Clamp(position.x, minX, maxX);  // Clamp X position within allowed boundaries
+            position.z = Mathf.Max(position.z, maxZ); // Ensure the Z position doesn't go beyond maxZ
+            transform.position = position;  // Apply the modified position back to the object
         
-        position.x = Mathf.Clamp(position.x, minX, maX);  
-        position.z = Mathf.Max(position.z, maxZ);
-       
     }
 }
