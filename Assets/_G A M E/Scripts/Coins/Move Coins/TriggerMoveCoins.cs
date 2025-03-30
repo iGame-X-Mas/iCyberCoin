21917a81b40e9de3df5e4dd4fad0d90a2925e7e1
🅰️🅻🅴🆇 🎮 <demian.x.mas@gmail.com>
2025-03-30 21:23:41 +0300

    Code Descriptions
    Added detailed descriptions of how the code works

--- Assets/_G A M E/Scripts/Coins/Move Coins/TriggerMoveCoins.cs
+++ Assets/_G A M E/Scripts/Coins/Move Coins/TriggerMoveCoins.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class TriggerMoveCoins : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 3f;
-    [SerializeField] private GameObject coin;
-    private bool shouldMove;
+    [SerializeField] private float moveSpeed = 3f; // Speed which the coin moves forward
+    [SerializeField] private GameObject coin;  // Reference the coin GameObject
+    private bool shouldMove; // Flag to check if the coin should start moving
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // When the player or ground enter the trigger set shouldMove true
     {
         if (other.CompareTag("Player") || other.CompareTag("Ground"))
             shouldMove = true;
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() // Moves the coin forward constant speed when shouldMove is true
     {
         if (shouldMove && coin)
             coin.transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime));
